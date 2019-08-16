f25942913d1806eb10c33225742eccc3015ab95e
jppavard <pavard.pub@gmail.com>
2019-08-16 13:39:35 +0200

    new program

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,5 @@
 class IngredientsController < ApplicationController
-   before_action :set_ingredient, only: %i[show]
+  before_action :set_ingredient, only: %i[show destroy]
 
   def index
     @ingredients = Ingredient.all
@@ -16,11 +16,10 @@ class IngredientsController < ApplicationController
   def create
     @ingredient = Ingredient.new(ingredient_params)
     if @ingredient.save
-      redirect_to cocktails_path, notice: 'Ingredient was successfully created.'
+      redirect_to ingredient_path(@ingredient), notice: 'Ingredient was successfully created.'
     else
       render :new
     end
-
   end
 
   def edit
@@ -30,16 +29,18 @@ class IngredientsController < ApplicationController
   end
 
   def destroy
+    @ingredient.destroy
+    redirect_to ingredients_path
   end
 
   private
- # Use callbacks to share common setup or constraints between actions.
+  # Use callbacks to share common setup or constraints between actions.
   def set_ingredient
     @ingredient = Ingredient.find(params[:id])
   end
 
   # Only allow a trusted parameter "white list" through.
   def ingredient_params
-    params.require(:ingredient).permit(:name)
+    params.require(:ingredient).permit(:name, :photo)
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
   has_many :doses
 
   validates :name, presence: true, uniqueness: { allow_blank: true, case_sensitive: false }
